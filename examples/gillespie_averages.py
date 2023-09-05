111892b1cff471976a59b85da3952a9a7ba0b4f0
Alessandro Lo Cuoco <80710565+locuoco@users.noreply.github.com>
2023-09-05 02:39:17 +0200

    Added graph of evolution of averages for single-substrate kinetics

--- examples/gillespie_averages.py
+++ examples/gillespie_averages.py
@@ -1,5 +1,5 @@
 '''
-    Stochastic enzyme kinetics: time completions histogram
+    Stochastic enzyme kinetics: Stochastic enzyme kinetics averages plot
     Copyright (C) 2023 Alessandro Lo Cuoco (alessandro.locuoco@gmail.com)
 
     This program is free software: you can redistribute it and/or modify
@@ -18,6 +18,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from statistics import mean, stdev
 import sys
 
@@ -39,31 +40,49 @@ g['Exact'] = gillespie.single_substrate(kf=kf, kb=kb, kcat=kcat, ET=ET, ST=ST)
 g['tQSSA'] = gillespie.single_substrate_tqssa(kM=kM, kcat=kcat, ET=ET, ST=ST)
 g['sQSSA'] = gillespie.single_substrate_sqssa(kM=kM, kcat=kcat, ET=ET, ST=ST)
 
-completion_times = {'Exact': [], 'tQSSA': [], 'sQSSA': []}
+dPs = {'Exact': [], 'tQSSA': [], 'sQSSA': []}
+ts  = {'Exact': [], 'tQSSA': [], 'sQSSA': []}
+n_simulations = {'Exact': 1000, 'tQSSA': 10000, 'sQSSA': 10000}
 init_conditions =  {'Exact': [0, 0], 'tQSSA': [0], 'sQSSA': [0]}
-
-n_simulations = 20000
-max_t = 20
+max_t = 9
 
 for s in sim:
-	for _ in range(n_simulations):
+	for _ in range(n_simulations[s]):
 		g[s].x = init_conditions[s]
 		g[s].t = 0
-		g[s].simulate(t_final=max_t, noreturn=True)
-		completion_times[s].append(g[s].t)
+		states = g[s].simulate(t_final=max_t)
+		prods = [state.x[g[s].species.P] for state in states]
+		dprods = np.diff(prods)
+		times = [state.t for state in states]
+		dPs[s] += dprods.tolist()
+		ts[s] += times[1:]
+
+ndiv = max_t*50 + 1
+bins = np.linspace(0, max_t, ndiv)
+
+dy = {}
+for s in sim:
+	dprods = np.asarray(dPs[s]) / n_simulations[s]
+	dy[s], _ = np.histogram(ts[s], bins, weights=dprods)
+
+y = {}
+var = {}
+dx = bins[1] - bins[0]
+for s in sim:
+	y[s] = np.cumsum(dy[s])
+	var[s] = y[s]*dy[s]/dx
+
+x = (bins[1:] + bins[:-1])/2
 
 for s in sim:
-	print(s, mean(completion_times[s]), '+/-', stdev(completion_times[s]))
+	error = np.sqrt(var[s])
 
-bins = np.linspace(0, 9, 19)
+	plt.plot(x, y[s], label=s)
+	plt.fill_between(x, 0, error, alpha=.3)
 
-plt.hist(completion_times['Exact'], bins, label='Exact', density=True, color='blue', histtype='step', fill=False)
-plt.hist(completion_times['tQSSA'], bins, label='tQSSA', density=True, color='red', alpha=.6)
-plt.hist(completion_times['sQSSA'], bins, label='sQSSA', density=True, color='gray', alpha=.3)
-
-plt.ylim(0, .4)
+plt.ylim(0)
 plt.xlabel('Time')
-plt.ylabel('Probability density')
+plt.ylabel('Products count (average)')
 plt.legend()
 
 plt.show()
